55590a97765cc3acf022e6c8407dd6e2e7a1384e
Alex <AlexWork@balphalala.local>
2019-06-17 11:29:30 -0500

    Updated WP_HOME and WP_SITEURL

--- wp-config.php
+++ wp-config.php
@@ -94,3 +94,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 /** Sets up WordPress vars and included files. */
 require_once( ABSPATH . 'wp-settings.php' );
+
+define('WP_HOME', 'http://192.168.0.3:8000/');
+define('WP_SITEURL', 'http://192.168.0.3:8000/');
